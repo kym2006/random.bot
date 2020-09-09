03a671ea61a2237c2120a83e5c0c11c662a482e4
kym2006 <kangyiming2006@gmail.com>
2020-09-09 14:09:03 +0800

    message on server join

--- cogs/events.py
+++ cogs/events.py
@@ -22,6 +22,16 @@ class Events(commands.Cog):
         embed.set_footer(text=f"{len(self.bot.guilds)} servers, {len(self.bot.users)} users")
         await self.bot.http.send_message(725303414916907043, None, embed=embed.to_dict())
         await self.bot.change_presence(activity=discord.Game("@help | @someone | being random on {} servers".format(len(self.bot.guilds))))
+        txtchannel = self.bot.get_channel(725303414363390018)
+        for i in guild.channels:
+            if i.type == txtchannel.type:
+                try:
+                    await i.send('''Thank you for inviting random.bot! Join our support server at https://discord.gg/ZatYnsX for monthly giveaways!
+Type @help to view all commands! 
+Type @leaderboard to see who's the richest users for random.bot! Note that the richer you are, the more likely you are to be chosen for the giveaway!''')
+                    return 
+                except:
+                    continue
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         embed = discord.Embed(
