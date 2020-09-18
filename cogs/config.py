f5e5ee715b15bef73808f7f7e6f93ccd48966406
kym2006 <kangyiming2006@gmail.com>
2020-09-18 14:32:56 +0800

    config.py

---
+++ cogs/config.py
@@ -0,0 +1,49 @@
+import discord 
+from discord.ext import commands 
+import random 
+import asyncio 
+import typing 
+
+# This cog implements a lot of the random functions of random.bot
+class Config(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.guild_only()
+    @commands.command(
+        description="Change the prefix or view the current prefix.", usage="prefix [new prefix]", aliases=["setprefix"]
+    )
+    async def prefix(self, ctx, *, prefix: str = None):
+        if prefix is None:
+            await ctx.send(
+                embed=discord.Embed(
+                    description=f"The prefix for this server is `{ctx.prefix}`.",
+                    colour=self.bot.primary_colour,
+                )
+            )
+            return
+        if ctx.author.guild_permissions.administrator is False:
+            await ctx.send("You need to be an admnistrator to do this.")
+            return 
+        else:
+            if len(prefix) > 10:
+                await ctx.send(
+                    embed=discord.Embed(
+                        description="The chosen prefix is too long.",
+                        colour=discord.Color.red()
+                    )
+                )
+                return
+            if prefix == '@':
+                prefix = None
+            await self.bot.conn.execute("UPDATE data SET prefix=$1 WHERE guild=$2", prefix, ctx.guild.id)
+            await ctx.send(
+                embed=discord.Embed(
+                    description="Successfully changed the prefix to "
+                    f"`{'@' if prefix is None else prefix}`.",
+                    colour=self.bot.primary_colour,
+                )
+            )
+
+def setup(bot):
+    bot.add_cog(Config(bot))
