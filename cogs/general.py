ff7e8c1de3dbddb23287fb9d4863d399dd3a71a0
kym2006 <kangyiming2006@gmail.com>
2020-09-30 20:49:36 +0800

    updated owners

--- cogs/general.py
+++ cogs/general.py
@@ -180,7 +180,7 @@ class General(commands.Cog):
         )
         embed.add_field(
             name="Owners",
-            value="kym2006#6342, SquiddyPoos#6795, waterflamev8#4123, FlareonIsCute#1566",
+            value="kym2006#6342, SquiddyPoos#6795",
         )
         embed.add_field(name="Bot Version", value=self.bot.version)
         embed.add_field(name="Uptime", value=self.get_bot_uptime(brief=True))
