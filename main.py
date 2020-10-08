e689ea2aea598b6d031f72d9791ef1ffa8671c01
kym2006 <kangyiming2006@gmail.com>
2020-10-08 13:50:47 +0800

    main branch

---
+++ main.py
@@ -0,0 +1,137 @@
+import asyncio
+import datetime
+import json
+import logging
+from pathlib import Path
+from config import token 
+from config import db_url
+import asyncpg
+import discord
+from discord.ext import commands
+from bot import RandomBot
+blacklist = []
+
+
+def config_load():
+    
+
+
+async def run():
+    """
+    Where the bot gets started. If you wanted to create an database connection pool or other session for the bot to use,
+    it's recommended that you create it here and pass it to the bot as a kwarg.
+    """
+
+    config = config_load()
+    bot = Bot(shard_count=10, config=config, description=config["description"])
+    bot.help_command = None
+    bot.primary_colour = discord.Color.green()
+
+    try:
+        await bot.start(config["token"])
+
+    except KeyboardInterrupt:
+        await bot.logout()
+
+
+class Bot(RandomBot):
+    def __init__(self, **kwargs):
+        super().__init__(command_prefix=self.get_prefix_, description=kwargs.pop("description"))
+        self.start_time = None
+        self.app_info = None
+        self.version = "1.0.0"
+        self.loop.create_task(self.track_start())
+        self.loop.create_task(self.load_all_extensions())
+
+
+    async def track_start(self):
+        """
+        Waits for the bot to connect to discord and then records the time.
+        Can be used to work out uptime.
+        """
+        await self.wait_until_ready()
+        self.start_time = datetime.datetime.utcnow()
+
+    async def get_prefix_(self, bot, message):
+        """
+        A coroutine that returns a prefix.
+
+        I have made this a coroutine just to show that it can be done. If you needed async logic in here it can be done.
+        A good example of async logic would be retrieving a prefix from a database.
+        """
+        prefix = "@"
+        if message.guild is None:
+            return commands.when_mentioned_or("@")(bot, message)
+        row = await self.conn.fetchrow("SELECT * FROM data WHERE guild = $1", message.guild.id)
+        if row is None or row["prefix"] is None:
+            prefix = "@"
+        else:
+            prefix = row["prefix"]
+        return commands.when_mentioned_or(prefix)(bot, message)
+
+    async def load_all_extensions(self):
+        """
+        Attempts to load all .py files in /cogs/ as cog extensions
+        """
+        await self.wait_until_ready()
+        await asyncio.sleep(1)  # ensure that on_ready has completed and finished printing
+        await self.change_presence(
+            activity=discord.Game("@help | @someone | being random on {} servers".format(len(self.guilds)))
+        )
+        cogs = [x.stem for x in Path("cogs").glob("*.py")]
+        for extension in cogs:
+            try:
+                self.load_extension(f"cogs.{extension}")
+                print(f"loaded {extension}")
+            except Exception as e:
+                error = f"{extension}\n {type(e).__name__} : {e}"
+                print(f"failed to load extension {error}")
+            print("-" * 10)
+
+    async def on_ready(self):
+        """
+        This event is called every time the bot connects or resumes connection.
+        """
+        print("-" * 10)
+        self.app_info = await self.application_info()
+        print(
+            f"Logged in as: {self.user.name}\n"
+            f"Using discord.py version: {discord.__version__}\n"
+            f"Owner: {self.app_info.owner}\n"
+            f"Template Maker: SourSpoon / Spoon#0001"
+        )
+        print("-" * 10)
+
+    async def getcommands(self):
+        await self.wait_until_ready()
+        await asyncio.sleep(2)
+        self.botcommands = []
+        for _, cog_name in enumerate(self.cogs):
+            cog = self.get_cog(cog_name)
+            cog_commands = cog.get_commands()
+            self.botcommands.extend(cog_commands)
+        print(self.botcommands)
+
+    async def on_message(self, message):
+        """
+        This event triggers on every message received by the bot. Including one's that it sent itself.
+
+        If you wish to have multiple event listeners they can be added in other cogs. All on_message listeners should
+        always ignore bots.
+        """
+        if message.author.bot:
+            return
+        if message.author.id in blacklist:
+            return
+        await self.process_commands(message)
+
+    @property
+    def uptime(self):
+        return datetime.datetime.utcnow() - self.start_time
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
---
+++ utils/checks.py
@@ -0,0 +1,113 @@
+import logging
+
+import discord
+from discord.ext import commands
+
+log = logging.getLogger(__name__)
+
+owners = [
+    298661966086668290,
+    412969691276115968,
+    446290930723717120,
+    488283878189039626,
+    685456111259615252,
+    723794074498367498,
+]
+admins = []
+
+
+def is_owner():
+    def predicate(ctx):
+        if ctx.author.id not in owners:
+            raise commands.NotOwner()
+        else:
+            return True
+
+    return commands.check(predicate)
+
+
+def is_admin():
+    def predicate(ctx):
+        if ctx.author.id not in admins and ctx.author.id not in owners:
+            raise commands.NotOwner()
+        else:
+            return True
+
+    return commands.check(predicate)
+
+
+def is_premium():
+    async def predicate(ctx):
+        async with ctx.bot.pool.acquire() as conn:
+            res = await conn.fetch("SELECT guild FROM premium")
+        all_premium = []
+        for row in res:
+            all_premium.extend(row[0])
+        if ctx.guild.id not in all_premium:
+            await ctx.send(
+                embed=discord.Embed(
+                    description="This server does not have premium. Want to get premium? More information "
+                    f"is available with the `{ctx.prefix}premium` command.",
+                    colour=ctx.bot.error_colour,
+                )
+            )
+            return False
+        else:
+            return True
+
+    return commands.check(predicate)
+
+
+def is_patron():
+    async def predicate(ctx):
+        async with ctx.bot.pool.acquire() as conn:
+            res = await conn.fetchrow(
+                "SELECT identifier FROM premium WHERE identifier=$1", ctx.author.id
+            )
+        if res:
+            return True
+        slots = await ctx.bot.tools.get_premium_slots(ctx.bot, ctx.author.id)
+        if slots is False:
+            await ctx.send(
+                embed=discord.Embed(
+                    description="This command requires you to be a patron. Want to become a patron? More "
+                    f"information is available with the `{ctx.prefix}premium` command.",
+                    colour=ctx.bot.error_colour,
+                )
+            )
+            return False
+        else:
+            async with ctx.bot.pool.acquire() as conn:
+                await conn.execute(
+                    "INSERT INTO premium (identifier, guild) VALUES ($1, $2)",
+                    ctx.author.id,
+                    [],
+                )
+            return True
+
+    return commands.check(predicate)
+
+
+def is_mod():
+    async def predicate(ctx):
+        has_role = False
+        roles = (await ctx.bot.get_data(ctx.guild.id))[3]
+        for role in roles:
+            role = ctx.guild.get_role(role)
+            if not role:
+                continue
+            if role in ctx.author.roles:
+                has_role = True
+                break
+        if has_role is False and ctx.author.guild_permissions.administrator is False:
+            await ctx.send(
+                embed=discord.Embed(
+                    description=f"You do not have access to use this command.",
+                    colour=ctx.bot.error_colour,
+                )
+            )
+            return False
+        else:
+            return True
+
+    return commands.check(predicate)
