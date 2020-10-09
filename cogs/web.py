496f57ddd84f45587f266e7d2a54afad925948a7
kym2006 <kangyiming2006@gmail.com>
2020-10-09 08:39:59 +0800

    update web

--- cogs/web.py
+++ cogs/web.py
@@ -21,27 +21,12 @@ class Webserver(commands.Cog):
         async def welcome(request):
             return web.Response(text="Hello, world")
 
-        @routes.post('/dbl')
-        async def dblwebhook(request):
-            if request.headers.get('authorization') == '3mErTJMYFt':
-                data = await request.json()
-                user = self.bot.get_user(data['user']) or await self.bot.fetch_user(data['user'])
-                if user is None:
-                    return
-                _type = f'Tested!' if data['type'] == 'test' else f'Voted!'
-                upvoted_bot = f'<@{data["bot"]}>'
-                embed = discord.Embed(title=_type, colour=discord.Color.blurple())
-                embed.description = f'**Upvoter :** {user.mention} Just {_type}' + f'\n**Upvoted Bot :** {upvoted_bot}'
-                embed.set_thumbnail(url=user.avatar_url)
-                channel = self.bot.get_channel(5645646545142312312)
-                await channel.send(embed=embed)
-            return 200
-
         @routes.get('/ping')
         async def ping(request):
             return web.Response(text=f"Ping is: {self.bot.latency*1000}")
 
         self.webserver_port = os.environ.get('PORT', 5000)
+        print(self.webserver_port)
         app.add_routes(routes)
 
         
@@ -50,7 +35,7 @@ class Webserver(commands.Cog):
     async def web_server(self):
         runner = web.AppRunner(app)
         await runner.setup()
-        site = web.TCPSite(runner, host='0.0.0.0', port=self.webserver_port)
+        site = web.TCPSite(runner, port=self.webserver_port)
         await site.start()
 
     @web_server.before_loop
