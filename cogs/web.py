8607e6baaf2e4c329f31593a466cf5cc082a30eb
kym2006 <kangyiming2006@gmail.com>
2020-10-11 11:13:47 +0800

    web.py

---
+++ cogs/web.py
@@ -0,0 +1,30 @@
+from aiohttp import web
+from discord.ext import commands, tasks
+import discord
+import os
+import aiohttp
+from quart import Quart
+app = web.Application()
+routes = web.RouteTableDef()
+
+
+
+
+class Webserver(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        app = Quart(__name__)
+        
+        @app.route('/')
+        async def hello():
+            return 'hello world'
+
+        @app.route('/ping')
+        async def ping():
+            return f"{self.bot.latency*1000}"
+
+        bot.loop.create_task(app.run_task())
+
+
+def setup(bot):
+    bot.add_cog(Webserver(bot))
