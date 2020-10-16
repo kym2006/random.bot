f8280d5399777a9dca8aebf7e30421b6d90bc5f7
kym2006 <kangyiming2006@gmail.com>
2020-10-16 20:53:00 +0800

    added colour

--- cogs/general.py
+++ cogs/general.py
@@ -143,11 +143,11 @@ class General(commands.Cog):
         await ctx.send(embed=page)
 
     @commands.command(description="Suggest a feature!", name="suggest", usage="suggest")
-    async def suggest(self, ctx, content:str):
+    async def suggest(self, ctx, *, content:str):
         channel=self.bot.get_channel(766642940323037214)
         embed=discord.Embed(description=content)
         embed.set_author(name=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=f"{ctx.author.avatar_url}")
-        await channel.send(embed=embed)
+        await channel.send(embed=embed, colour=bot.config.primary_colour)
 
     @commands.command(description="Look at my partners", usage="partners")
     async def partners(self, ctx):
