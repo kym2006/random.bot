62f98fb463d5729d688f24ac39079bce85b83aad
Kang Yiming <kangyiming2006@gmail.com>
2020-11-02 19:55:17 +0800

    updated org

--- cogs/org.py
+++ cogs/org.py
@@ -14,10 +14,17 @@ class Org(commands.Cog):
         self.bot = bot
 
     #@checks.is_patron()
-    @commands.group()
+    @commands.group(usage="?org", description="A group of commands whereby random.org api is used to generate more cryptographically secure results")
     async def org(self,ctx):
         if ctx.invoked_subcommand is None:
-            await ctx.send(embed=discord.Embed(description=f"Invalid command! Do `{ctx.prefix}help` or `{ctx.prefix}commands` to find out the commands!"))
+            cog=self.bot.get_cog("Org")
+            commands=[c.qualified_name for c in cog.walk_commands()]
+            res="```\nCommands using random.org api:\n"
+            for i in commands:
+                res+=i+'\n'
+            res+='```'
+            await ctx.send(embed=discord.Embed(description=res,colour=self.bot.config.primary_colour))
+            
     
     async def get_data(self,payload):
         async with aiohttp.ClientSession() as session:
@@ -26,7 +33,7 @@ class Org(commands.Cog):
                 return js['result']
 
     @checks.is_patron()
-    @org.command()
+    @org.command(usage="randint <start> <end>", description="Get a random integer from <start> to <end>, using random.org api")
     async def randint(self,ctx,start:int,end:int):
         payload={"jsonrpc":"2.0","method":"generateIntegers","params":{"apiKey":self.bot.config.randomorg,"n":1,"min":start,"max":end,"replacement":True,"base":10},"id":29440}
         res=await self.get_data(payload)
