0541c8aaeee2274a4912d1c97e985dd8d97037ad
Kang Yiming <kangyiming2006@gmail.com>
2020-12-01 12:59:49 +0800

    add monthly reset

---
+++ monthlyreset.py
@@ -0,0 +1,20 @@
+import asyncio 
+import aiohttp 
+import json 
+import asyncpg 
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+db = os.getenv("DB_URL")
+async def run():
+    conn = await asyncpg.connect(db)
+    res = await conn.fetch("SELECT * FROM credit")
+    for i in res:
+        silver = i['silver']
+        curgold = i['gold']
+        await conn.execute("UPDATE credit set gold=$1 where userid=$2", curgold + (19+silver)//20, i['userid'])
+    await conn.execute("UPDATE credit set silver=0")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
+
