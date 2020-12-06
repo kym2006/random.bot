4db32108130000bd0c3c44a318fd9801c6c1362e
Kang Yiming <kangyiming2006@gmail.com>
2020-12-06 08:47:41 +0800

    updated error handling

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -14,8 +14,6 @@ class ErrorHandler(commands.Cog):
         self.client = None
 
     async def _on_command_error(self, ctx, error, bypass=False):
-        channel =self.bot.get_channel(782156413090529301)
-        await channel.send(embed=discord.Embed(title="Error!", description=f"Content:{ctx.message.content}\nAuthor:{ctx.message.author.id}\n", colour=discord.Color.red()))
         if (
             hasattr(ctx.command, "on_error")
             or (ctx.command and hasattr(ctx.cog, f"_{ctx.command.cog_name}__error"))
@@ -24,7 +22,9 @@ class ErrorHandler(commands.Cog):
             return
         if isinstance(error, commands.CommandNotFound):
             return
-        elif isinstance(error, commands.NoPrivateMessage):
+        channel =self.bot.get_channel(782156413090529301)
+        await channel.send(embed=discord.Embed(title="Error!", description=f"Content:{ctx.message.content}\nAuthor:{ctx.message.author.id}\n", colour=discord.Color.red()))
+        if isinstance(error, commands.NoPrivateMessage):
             await ctx.send(
                 embed=discord.Embed(
                     title="Command Unavailable",
