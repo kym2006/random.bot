1edc05d84f566b682c702468eed784bbb81b2af8
Kang Yiming <kangyiming2006@gmail.com>
2021-11-24 16:18:56 +0800

    colour.js

---
+++ commands/random/colour.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const { sample } = require('../../utils/tools');
+
+function randomColour() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+      .setName('colour')
+      .setDescription('Get a random colour'),
+
+    info: {
+      module: 'colour',
+      permLevel: 0,
+      usage: 'colour'
+    },
+    async execute(interaction) {
+        //get a random RGB colour 
+        const colour = randomColour();
+        const embed = new MessageEmbed()
+            .setColor(colour)
+            .setTitle('Colour')
+            .setDescription(`${colour}`);
+
+        console.log(embed)
+        await interaction.reply(embed);
+        
+      }
+    };
