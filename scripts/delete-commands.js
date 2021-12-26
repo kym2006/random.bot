6e854fa2a7656545fa599fb82b19cc907b420457
Ang Jun Ray <angjunray@live.com>
2021-12-26 14:13:21 +0800

    move scripts into a folder and add delete-commands

---
+++ scripts/delete-commands.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const env = require('process').env;
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { config } = require('../dist/src/utils/config.js');
+
+// NOTE: The directory "commands" should contain subdirectories to organise commands.
+const commandFiles = fs
+  .readdirSync('./dist/src/commands')
+  .map(file => path.join('./dist/src/commands', file))
+  .filter(file => fs.lstatSync(file).isDirectory())
+  .map(dir =>
+    fs
+      .readdirSync(dir)
+      .filter(file => file.endsWith('.js'))
+      .map(file => path.join(dir, file))
+  );
+
+const rest = new REST({ version: '9' }).setToken(config.tokenId);
+
+// delete slash commands
+rest
+  .get(Routes.applicationCommands(config.clientId))
+  .then(data => {
+    const promises = [];
+    for (const command of data) {
+      const deleteUrl = `${Routes.applicationCommands(config.clientId)}/${command.id}`;
+      promises.push(
+        (async () => {
+          return rest.delete(deleteUrl).then(() => console.log(`Deleting ${command.name}.`));
+        })()
+      );
+    }
+    return Promise.all(promises);
+  })
+  .then(() => console.log('Successfully deleted all commands.'));
---
+++ scripts/deploy-commands.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const env = require('process').env;
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { config } = require('../dist/src/utils/config.js');
+
+const commands = [];
+
+// NOTE: The directory "commands" should contain subdirectories to organise commands.
+const commandFiles = fs
+  .readdirSync('./dist/src/commands')
+  .map(file => path.join('./dist/src/commands', file))
+  .filter(file => fs.lstatSync(file).isDirectory())
+  .map(dir =>
+    fs
+      .readdirSync(dir)
+      .filter(file => file.endsWith('.js'))
+      .map(file => path.join(dir, file))
+  );
+
+for (const filecol of commandFiles) {
+  for (const name of filecol) {
+    const command = require(`../${name}`); // funny path-fu
+    commands.push(command.default.data.toJSON());
+    console.log(`Registered ${name}.`);
+  }
+}
+
+const rest = new REST({ version: '9' }).setToken(config.tokenId);
+
+// register slash commands
+(async () => {
+  try {
+    if (env.NODE_ENV === 'production') {
+      console.log('Deploying commands globally.');
+      await rest.put(Routes.applicationCommands(config.clientId), {
+        body: commands
+      });
+    } else {
+      console.log('Deploying commands locally onto Beta.');
+      await rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), {
+        body: commands
+      });
+    }
+
+    console.log('Successfully registered application commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
